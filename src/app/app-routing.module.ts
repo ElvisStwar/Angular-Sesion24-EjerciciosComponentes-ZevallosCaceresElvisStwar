0277810032d4aac935c0a748bdff74da05da3896
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-26 12:28:42 -0500

    Cracion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"",component:NavigationComponent,
+    children:[
+      {path:"dashboard", component:DashboardComponent},
+      {path:"dialog", component:DialogComponent}
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    DashboardComponent
+    DashboardComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
